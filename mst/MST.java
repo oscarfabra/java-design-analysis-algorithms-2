b16db60fa76c664ffe593ffdf66b013126aa1c83
oscarfabra <oscarfabra@gmail.com>
2014-07-03 12:15:41 -0500

    Prim's MST algorithm initial commit

---
+++ mst/MST.java
@@ -0,0 +1,32 @@
+/**
+ * $Id: MST.java, v 1.0 03/07/14 20:38 oscarfabra Exp $
+ * {@code MST} Class that implements a greedy algorithm for computing the
+ * minimum spanning tree of a given undirected graph.
+ *
+ * @author <a href="mailto:oscarfabra@gmail.com">Oscar Fabra</a>
+ * @version 1.0
+ * @since 03/07/14
+ */
+
+
+/**
+ * Class that implements a greedy algorithm for computing the minimum spanning
+ * tree of a given undirected graph.
+ */
+public class MST
+{
+    //-------------------------------------------------------------------------
+    // CONSTRUCTOR
+    //-------------------------------------------------------------------------
+
+    private MST(){ }        // This class shouldn't be instantiated
+
+    //-------------------------------------------------------------------------
+    // PUBLIC CLASS METHODS
+    //-------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------
+    // PRIVATE HELPER METHODS
+    //-------------------------------------------------------------------------
+
+}
