fe0a37d6eb088735256ff8be5c354b91a7669b81
Oscar Fabra <oscarfabra@gmail.com>
2014-07-19 23:11:04 -0500

    Clustering algorithm initial commit

---
+++ clustering/Solver.java
@@ -0,0 +1,104 @@
+/**
+ * $Id: Solver.java, v 1.0 17/07/14 21:14 oscarfabra Exp $
+ * {@code Solver} Is a class that reads and solves a greedy algorithm for
+ * computing a max-spacing k-clustering.
+ *
+ * @author <a href="mailto:oscarfabra@gmail.com">Oscar Fabra</a>
+ * @version 1.0
+ * @since 17/07/14
+ */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that reads and solves a greedy algorithm for computing a max-spacing
+ * k-clustering.
+ */
+public class Solver
+{
+    //-------------------------------------------------------------------------
+    // PUBLIC METHODS
+    //-------------------------------------------------------------------------
+
+    /**
+     * Solves the given instance and prints the solution in standard output.
+     * @param lines Input list with the variables for the problem.
+     */
+    private static void solve(List<String> lines)
+    {
+        // TODO: Write the clustering algorithm...
+    }
+
+    /**
+     * Reads the lines received from standard input and arranges them in a
+     * list.
+     * @param args Array of String with the filepath of the file to read.
+     * @return A list of lines with the data to solve the problem.
+     * @throws java.io.FileNotFoundException If file couldn't be found.
+     */
+    private static List<String> readLines(String[] args)
+            throws FileNotFoundException
+    {
+        List<String> lines = new ArrayList<String>();
+        String filename = null;
+
+        // Gets the file name
+        String arg = args[0];
+        if(arg.startsWith("-file="))
+        {
+            filename = arg.substring(6);
+        }
+
+        if(filename == null)
+        {
+            return null;
+        }
+
+        // Reads the lines out of the file
+        FileReader fileReader = new FileReader(filename);
+        BufferedReader buffer = new BufferedReader(fileReader);
+        String line = null;
+        try
+        {
+            while((line = buffer.readLine()) != null)
+            {
+                lines.add(line);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        // Returns the lines read
+        return lines;
+    }
+
+    //-------------------------------------------------------------------------
+    // MAIN
+    //-------------------------------------------------------------------------
+
+    /**
+     * Main test method.
+     * @param args filepath relative to the file with the representation of an
+     *             undirected graph in the form -file=filepath
+     */
+    public static void main(String [] args)
+    {
+        List<String> lines = null;
+        try
+        {
+            lines = Solver.readLines(args);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        Solver.solve(lines);
+    }
+}
