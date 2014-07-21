cc27fd711fea06e92a2d007a59a852be28f5b7e1
Oscar Fabra <oscarfabra@gmail.com>
2014-07-20 22:43:23 -0500

    Architecting max-spacing k-clustering greedy alg

--- clustering/Solver.java
+++ clustering/Solver.java
@@ -9,11 +9,11 @@
  */
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Class that reads and solves a greedy algorithm for computing a max-spacing
@@ -21,6 +21,13 @@ import java.util.List;
  */
 public class Solver
 {
+    //-------------------------------------------------------------------------
+    // CLASS VARIABLES
+    //-------------------------------------------------------------------------
+
+    // Number of clusters desired read from standard input
+    private static int k;
+
     //-------------------------------------------------------------------------
     // PUBLIC METHODS
     //-------------------------------------------------------------------------
@@ -31,7 +38,24 @@ public class Solver
      */
     private static void solve(List<String> lines)
     {
-        // TODO: Write the clustering algorithm...
+        // Gets the number of vertices or nodes n, and the number of edges m
+        int n = Integer.parseInt(lines.remove(0));
+        int m = lines.size();
+
+        // Gets the map of distances between each node and the other nodes
+        Map<Integer, List<Edge>> vertexEndpoints =
+                Graph.buildVertexEdges(m, lines);
+
+        // Creates a new graph to solve the k-clustering problem
+        Graph graph = new Graph(n, vertexEndpoints);
+
+        // Finds the max-spaacing of the k-clustering of the distance function
+        // represented in the graph
+        int maxSpacing = Clustering.findMaxSpacing(graph,Solver.k);
+
+        // Prints solution in standard output
+        System.out.println("The max-spacing of a " + Solver.k +
+                "-clustering is: "+ maxSpacing);
     }
 
     /**
@@ -39,10 +63,10 @@ public class Solver
      * list.
      * @param args Array of String with the filepath of the file to read.
      * @return A list of lines with the data to solve the problem.
-     * @throws java.io.FileNotFoundException If file couldn't be found.
+     * @throws Exception If file couldn't be found or k not given.
      */
     private static List<String> readLines(String[] args)
-            throws FileNotFoundException
+            throws Exception
     {
         List<String> lines = new ArrayList<String>();
         String filename = null;
@@ -57,6 +81,16 @@ public class Solver
         if(filename == null)
         {
             return null;
+        }
+
+        // Reads second argument, throws exception if not an integer
+        try
+        {
+            Solver.k = Integer.parseInt(args[1]);
+        }
+        catch(NumberFormatException nfe)
+        {
+            throw nfe;
         }
 
         // Reads the lines out of the file
@@ -87,6 +121,7 @@ public class Solver
      * Main test method.
      * @param args filepath relative to the file with the representation of an
      *             undirected graph in the form -file=filepath
+     *             Number of clusters desired k.
      */
     public static void main(String [] args)
     {
@@ -95,7 +130,7 @@ public class Solver
         {
             lines = Solver.readLines(args);
         }
-        catch (FileNotFoundException e)
+        catch (Exception e)
         {
             e.printStackTrace();
         }
