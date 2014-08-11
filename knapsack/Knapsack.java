78b9c9190d3c21ee782530c9ee452d0e5bacfbda
Oscar Fabra <oscarfabra@gmail.com>
2014-08-10 21:04:57 -0500

    Architected knapsack problem's solution, about 2 write straightforward impl

--- knapsack/Knapsack.java
+++ knapsack/Knapsack.java
@@ -1,14 +1,22 @@
 /**
  * $Id: Knapsack.java, v1.0 9/08/14 05:38 PM oscarfabra Exp $
- * {@code Knapsack}
+ * {@code Knapsack} Solves an instance of the popular knapsack problem.
  *
  * @author <a href="mailto:oscarfabra@gmail.com">Oscar Fabra</a>
  * @version 1.0
  * @since 9/08/14
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Knapsack
+ * Knapsack is a class that solves the knapsack problem defined as follows:
+ * Input: n items. Each has attributes: value v_i (non-negative), size w_i
+ * (non-negative and integral), capacity W (a non-negative integer).
+ * Output: A subset S of {1, 2, ..., n} that:
+ * Maximizes: Sum of v_i's, for all i in S
+ * Subject to: Sum of w_i's are upper bounded by W, for all i in S.
  */
 public class Knapsack
 {
@@ -16,16 +24,102 @@ public class Knapsack
     // ATTRIBUTES
     //-------------------------------------------------------------------------
 
+    // Constant to determine whether to solve the problem by a straightforward
+    // manner or optimized for big numbers.
+    private static final int threshold = 1000000;
+
+    // TODO: Add data structures to improve performance for BIG n...
+
     //-------------------------------------------------------------------------
     // CONSTRUCTORS
     //-------------------------------------------------------------------------
+
+    private Knapsack() {}       // This class shouldn't be instantiated
 
     //-------------------------------------------------------------------------
     // PUBLIC METHODS
     //-------------------------------------------------------------------------
 
+    /**
+     * Builds and returns a list of strings with the value and weight of each
+     * item and number of items n.
+     * <b>Pre:</b> Each line of the given list of string has the form:
+     * [value_i] [weight_i] for each i in {1...n}
+     * @param lines List of string with the attributes for each item.
+     * @param n Number of items.
+     * @return List of items for a particular knapsack problem.
+     */
+    public static List<Item> buildItems(List<String> lines, int n)
+    {
+        List<Item> items = new ArrayList<Item>(n);
+        int newItemId = 1;
+        for(String line : lines)
+        {
+            String [] valueAndWeight = line.split(" ");
+            int value = Integer.parseInt(valueAndWeight[0]);
+            int weight = Integer.parseInt(valueAndWeight[1]);
+            Item item = new Item(newItemId++, value, weight);
+            items.add(item);
+        }
+        return items;
+    }
+
+    /**
+     * Solves the given instance of the knapsack problem.
+     * @param items List of items.
+     * @param W Knapsack size.
+     * @param n Number of items.
+     * @return Value of the optimal solution, leaving the selected items in the
+     * given items list.
+     */
+    public static int solve(List<Item> items, int W, int n)
+    {
+        // Determines whether to solve the problem using a straightforward
+        // implementation or one that's optimized for big numbers
+        int value = 0;
+        if(Knapsack.threshold >= W * n)
+        {
+            value = Knapsack.solveStraightforward(items, W, n);
+        }
+        else
+        {
+            value = Knapsack.solveForBigData(items, W, n);
+        }
+        // Returns the value of the optimal solution
+        return value;
+    }
+
     //-------------------------------------------------------------------------
     // PRIVATE HELPER METHODS
     //-------------------------------------------------------------------------
 
+    /**
+     * Solves the knapsack problem using a straightforward implementation.
+     * @param items List of items.
+     * @param W Knapsack size.
+     * @param n Number of items.
+     * @return Value of the optimal solution, leaving the selected items in the
+     * given items list.
+     */
+    private static int solveStraightforward(List<Item> items, int W, int n)
+    {
+        // TODO: Straightforward solution...
+
+        return 0;
+    }
+
+    /**
+     * Solves the knapsack problem using an implementation optimized for big n.
+     * @param items List of items.
+     * @param W Knapsack size.
+     * @param n Number of items.
+     * @return Value of the optimal solution, leaving the selected items in the
+     * given items list.
+     */
+    private static int solveForBigData(List<Item> items, int W, int n)
+    {
+        // TODO: Straightforward solution...
+
+        return 0;
+    }
 }
--- knapsack/Solver.java
+++ knapsack/Solver.java
@@ -1,27 +1,123 @@
 /**
  * $Id: Solver.java, v1.0 9/08/14 05:36 PM oscarfabra Exp $
- * {@code Solver}
+ * {@code Solver} Is a class that reads and solves an instance of the popular
+ * knapsack problem using a dynamic programming approach.
  *
  * @author <a href="mailto:oscarfabra@gmail.com">Oscar Fabra</a>
  * @version 1.0
  * @since 9/08/14
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Solver
+ * Solver is a class that reads and solves an instance of the popular knapsack
+ * problem using a dynamic programming approach.
  */
 public class Solver
 {
     //-------------------------------------------------------------------------
-    // PUBLIC METHODS
+    // PRIVATE METHODS
     //-------------------------------------------------------------------------
 
-    //-------------------------------------------------------------------------
-    // PRIVATE HELPER METHODS
-    //-------------------------------------------------------------------------
+    /**
+     * Solves the given instance and prints the solution in standard output.
+     * @param lines Input list with the variables for the problem.
+     */
+    private static void solve(List<String> lines)
+    {
+        // Gets knapsack size and number of items from first line
+        String firstLine = lines.remove(0);
+        String [] wAndN = firstLine.split(" ");
+        int W = Integer.parseInt(wAndN[0]);
+        int n = Integer.parseInt(wAndN[1]);
+
+        // Builds a list of items as an input to solve the problem
+        List<Item> items = Knapsack.buildItems(lines, n);
+
+        // Finds the value of the optimal solution given the list of items and
+        // knapsack size; leaves the items to select in the items list
+        int value = Knapsack.solve(items, W, n);
+
+        // Prints the value of the optimal solution and the solution itself
+        System.out.println("The value of the optimal solution is: " + value);
+        System.out.println("The items to select are the following: ");
+        for(int i = 0; i < items.size(); i++)
+        {
+            System.out.println(items.get(i).getId());
+            System.out.println((i < items.size() - 1) ? ", " : ".");
+        }
+    }
+
+    /**
+     * Reads the lines received from standard input and arranges them in a
+     * list.
+     * @param args Array of String with the filepath of the file to read.
+     * @return A list of lines with the data to solve the problem.
+     * @throws Exception If file couldn't be found or k not given.
+     */
+    private static List<String> readLines(String[] args)
+            throws Exception
+    {
+        List<String> lines = new ArrayList<String>();
+        String filename = null;
+
+        // Gets the file name
+        String arg = args[0];
+        if(arg.startsWith("-file="))
+        {
+            filename = arg.substring(6);
+        }
+
+        if(filename == null)
+        {
+            return null;
+        }
+
+        // Reads the lines out of the file
+        FileReader fileReader = new FileReader(filename);
+        BufferedReader buffer = new BufferedReader(fileReader);
+        String line = null;
+        try
+        {
+            while((line = buffer.readLine()) != null)
+            {
+                lines.add(line);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        // Returns the lines read
+        return lines;
+    }
 
     //-------------------------------------------------------------------------
     // MAIN
     //-------------------------------------------------------------------------
 
+    /**
+     * Main test method.
+     * @param args filepath relative to the file with the representation of an
+     *             undirected graph in the form -file=filepath
+     */
+    public static void main(String [] args)
+    {
+        List<String> lines = null;
+        try
+        {
+            lines = Solver.readLines(args);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        Solver.solve(lines);
+    }
 }
