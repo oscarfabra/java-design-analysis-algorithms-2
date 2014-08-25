5cff002a292b15033071d633639090701cb1288b
Oscar Fabra <oscarfabra@gmail.com>
2014-08-24 22:59:01 -0500

    Some comments added.

--- dijkstra/Dijkstra.java
+++ dijkstra/Dijkstra.java
@@ -1,7 +1,7 @@
 /**
  * $Id: Dijkstra.java, v 1.0 24/06/14 21:50 oscarfabra Exp $
  * {@code Dijkstra} Is an implementation of Dijkstra's shortest path
- * algorithm. <br/>
+ * algorithm for directed graphs with no negative edges. <br/>
  *
  * @author <a href="mailto:oscarfabra@gmail.com">Oscar Fabra</a>
  * @version 1.0
@@ -63,6 +63,7 @@ public class Dijkstra
      * Solves Dijkstra's shortest path algorithm starting on vertex with id s
      * in the given graph object, s in [1...n], where n is the number of
      * vertices in graph.
+     <b>Pre:</b> Given directed graph has no negative edges.
      * @param s Id of the starting vertex in graph.
      * @param graph Graph to search the shortest path for.
      * @return List of shortest distances from s to each of the other vertices.
